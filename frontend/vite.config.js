63265cdfa07843e6c31cb2eb76551ba63cebf4de
harshpatidar245 <harshpatidar2400@gmail.com>
2025-07-19 16:19:34 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
-  server: {
-    allowedHosts: 'all', // Allows all external hosts like ngrok
-  },
+export default defineConfig(({ mode }) => {
+  // Load env variables
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    base: './',
+    server: {
+      allowedHosts: 'all',
+    },
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+  }
 })
